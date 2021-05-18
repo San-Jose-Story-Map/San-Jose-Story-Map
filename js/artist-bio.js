0ffe7aa04ad5d5bd0d31598ffbf29f24cd81f75b
Angelabelle Abarientos <34222276+angelabelle@users.noreply.github.com>
2021-05-17 18:52:52 -0600

    Small Bios

--- js/artist-bio.js
+++ js/artist-bio.js
@@ -1,39 +1,25 @@
 var geojsonData;
-
-// Store local geojson file into a variable
+// Gathers artist details from geojson and stores it in variable
 $(document).ready(function() {
-  //thinking that we manally update the geojson file
+  //change the file when new submissions have been made
   $.getJSON('data/2021-05-03-features.geojson', function(results) {
-    // Assign the results to the geojsonData variable
     geojsonData = results;
-      // console.log(geojsonData);
+    geojsonData.features.forEach(function(artist, i) {
+      var prop = artist.properties;
+      prop.id = i;
 
-        geojsonData.features.forEach(function(artist, i) {
-          var prop = artist.properties;
-          prop.id = i;
+      //If the user has submitted a bio, create element onto the page
+      if (prop["bio url"]) {
+        var listing = document.createElement('section');
+        document.body.appendChild(listing);
 
-          var listing = document.createElement('section');
-          document.body.appendChild(listing);
-          listing.className = 'page-section';
-          listing.id = prop['first name'] + prop['last name'];
-          listing.innerHTML = "<div class='container'><div class='text-center'><h2 class='section-heading text-uppercase'>" + prop['first name'] + " " + prop['last name'] + "</h2>";
+        listing.className = 'page-section';
+        listing.id = prop['first name'] + prop['last name'];
+        listing.innerHTML = "<div class='container'><div class='text-center'><h2 class='section-heading text-uppercase'>" + prop['first name'] + " " + prop['last name'] + "</h2>";
 
-          var details = listing.appendChild(document.createElement('p'));
-          if (prop["bio url"]) {
-            details.innerHTML += prop["bio url"] + "</div></div>";
-          }
-
-        });
-
+        var details = listing.appendChild(document.createElement('p'));
+        details.innerHTML += prop["bio url"] + "</div></div>";
+      }
+    });
   });
-
-
-
-
-
 });
-
-
-
-
-// console.log (geojsonData.)
