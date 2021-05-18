1354b2c781e7cb9ce0f0fc3d082f4af901245c86
Angelabelle Abarientos <34222276+angelabelle@users.noreply.github.com>
2021-05-17 18:15:21 -0600

    Arist-bio

--- js/artist-bio.js
+++ js/artist-bio.js
@@ -6,34 +6,21 @@ $(document).ready(function() {
   $.getJSON('data/2021-05-03-features.geojson', function(results) {
     // Assign the results to the geojsonData variable
     geojsonData = results;
-    //for testing
-    // console.log(results);
-    //assign each artist to an ID
-      console.log(geojsonData);
-
-
-
-
-
+      // console.log(geojsonData);
 
         geojsonData.features.forEach(function(artist, i) {
           var prop = artist.properties;
           prop.id = i;
 
-          // Add a new listing section to the sidebar.
-          var listings = document.getElementById('page-section-proto');
-          var listing = listings.appendChild(document.createElement('section'));
-          // Assign a unique `id` to the listing.
-          listing.id = "#" + prop['first name'] + prop['last name'];
-          // Assign the `item` class to each listing for styling.
-          listing.className = 'artist-bios';
-
-          //using the title of the piece as header
-          listing.innerHTML = "<div class='text-center'><h2 class='section-heading text-uppercase'>" + prop['first name'] + " " + prop['last name'] + "</h2>";
+          var listing = document.createElement('section');
+          document.body.appendChild(listing);
+          listing.className = 'page-section';
+          listing.id = prop['first name'] + prop['last name'];
+          listing.innerHTML = "<div class='container'><div class='text-center'><h2 class='section-heading text-uppercase'>" + prop['first name'] + " " + prop['last name'] + "</h2>";
 
           var details = listing.appendChild(document.createElement('p'));
           if (prop["bio url"]) {
-            details.innerHTML += prop["bio url"] + "</div>";
+            details.innerHTML += prop["bio url"] + "</div></div>";
           }
 
         });
